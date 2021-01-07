82b93dfb75481dc7544d5fc6de3ffa2c79efdccf
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-01-07 19:42:39 +0100

    logistic regression

--- 5_machine_learning_basics/5_7_1_logistic regression.py
+++ 5_machine_learning_basics/5_7_1_logistic regression.py
@@ -32,14 +32,21 @@ class Net(nn.Module):
 
 X = torch.tensor(X)
 y = torch.tensor(y)
-trainset = torch.utils.data.TensorDataset(X, y)
+split_rate = 0.8
+wholeset = torch.utils.data.TensorDataset(X, y)
+train_len = int(.8 * len(wholeset))
+test_len = len(wholeset) - train_len
+trainset, testset = torch.utils.data.random_split(wholeset, [train_len, test_len])
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=0)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                          shuffle=False, num_workers=0)
 
 net = Net()
 
 import torch.optim as optim
 
+# train 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
@@ -68,3 +75,28 @@ for epoch in range(2):  # loop over the dataset multiple times
             running_loss = 0.0
 
 print('Finished Training')
+
+# test
+X_hats, lbls = [], []
+net.eval()
+with torch.no_grad():
+    running_loss = 0.0
+    for i, data in enumerate(testloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs.float())
+        X_hats.append(inputs.numpy())
+        lbls.append(labels.numpy().reshape(-1,1))
+        
+lbls = np.vstack(lbls)
+X_hats = np.vstack(X_hats)
+# plot classification
+y_plot = y.reshape(-1, 1)
+dataframe = pd.DataFrame(data=np.hstack([X_hats,lbls]), columns=("1st_principal", "2nd_principal", "label"))
+sn.FacetGrid(dataframe, hue="label", size=6).map(plt.scatter, '1st_principal', '2nd_principal').add_legend()
+plt.show()
