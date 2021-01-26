a78ce1e37418b9c8951211f0b59a57553fbe8a49
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-01-26 19:49:10 +0100

    fix drawing

--- 5_machine_learning_basics/5_7_2_SVM.py
+++ 5_machine_learning_basics/5_7_2_SVM.py
@@ -9,7 +9,7 @@ from scipy.optimize import minimize
 
 from scipy.optimize import NonlinearConstraint
 
-
+## make blobs
 N = 100
 X, y = make_blobs(n_samples=N, centers=2, n_features=2, random_state=2)
 y[y==0] = -1
@@ -22,6 +22,7 @@ plt.scatter(X[2][0], X[2][1], color='r')
 plt.scatter(X[44][0], X[44][1], color='r')
 plt.show()
 
+## optimization
 alphs0 = np.ones_like(y)
 
 def func(alphs, X, y):
@@ -33,7 +34,7 @@ def func(alphs, X, y):
             sum2 += alphs[i]*alphs[j]*y[i]*y[j]*X[i]@X[j]
     sum2 /= 2
     ret = sum1 - sum2
-    print(ret)
+    # print(ret)
     return - ret
 
 bounds = [[0, np.inf] for _ in range(N)]
@@ -52,13 +53,15 @@ res = minimize(func, alphs0, (X, y), tol=1e-5, bounds=bounds, constraints=cons,
                options=options)
 # res = minimize(func, alphs0, (X, y), tol=1e-2)
 
-xx = np.arange(-7, 7, 0.1)
-yy = np.arange(-12, 2, 0.1)
+## draw decision boundary
+
+xx = np.arange(-7, 7, .1)
+yy = np.arange(-12, 2, .1)
 
 
 xxx, yyy = np.meshgrid(xx, yy, sparse=False)
 
-arr = np.zeros((140,140))
+arr = np.zeros((len(xx),len(yy)))
 
 alphs_opt = np.round(res.x.reshape((-1,1)), 2)
 
@@ -68,28 +71,20 @@ for i in range(len(y)):
     
 b = -(w.T@X[2] + w.T@X[44])[0]/2
 
-for i in range(len(y)):
-    for j in range(len(y)):
+for i in range(len(xx)):
+    for j in range(len(yy)):
         arr[i][j] = (w.T@np.array([xxx[i][j], yyy[i][j]]) + b) > 0
         # arr[i][j] = ((alphs_opt[i]*y[i]*X[i]@np.array([xxx[i][j], yyy[i][j]])) + b) > 0
-        
-
+    
+    
 dataframe = pd.DataFrame(data=np.hstack([np.vstack((xxx.ravel(), yyy.ravel())).T, arr.ravel().reshape(-1, 1)]), columns=("x", "y", "label"))
-sn.FacetGrid(dataframe, hue="label", size=6).map(plt.scatter, 'x', 'y').add_legend()
+sn.FacetGrid(dataframe, hue="label", palette='pastel', size=6).map(plt.scatter, 'x', 'y').add_legend()
 dataframe = pd.DataFrame(data=np.hstack([X,y_plot]), columns=("x", "y", "label"))
 sn.FacetGrid(dataframe, hue="label", size=6).map(plt.scatter, 'x', 'y').add_legend()
 plt.scatter(X[2][0], X[2][1], color='r')
 plt.scatter(X[44][0], X[44][1], color='r')
 plt.scatter(w[0], w[1], color='green', s=100)
 x_min, x_max = plt.xlim()
-plt.plot((x_min, x_max), ((-b-w[0]*x_min)/w[1], (-b-w[0]*x_max)/w[1]))
+plt.plot((x_min, x_max), ((-b-w[0]*x_min)/w[1], (-b-w[0]*x_max)/w[1]), color='red')
 plt.show()
 
-
-# y_hat = []
-
-# for i in range(len(y)):
-#     print(alphs[i]*y[i]*X[[i]]@X[i])
-
-# for i in range(len(y)):
-#     y_hat.append(alphs[i]*y[i]*X[[i]]@X[i])
