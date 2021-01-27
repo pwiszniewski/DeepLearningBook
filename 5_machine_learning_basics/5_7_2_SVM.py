3f94d9d49c60fba1112c739d0d7430c056ce315d
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-01-27 20:08:45 +0100

    improve plot

--- 5_machine_learning_basics/5_7_2_SVM.py
+++ 5_machine_learning_basics/5_7_2_SVM.py
@@ -10,16 +10,14 @@ from scipy.optimize import minimize
 from scipy.optimize import NonlinearConstraint
 
 ## make blobs
-N = 100
-X, y = make_blobs(n_samples=N, centers=2, n_features=2, random_state=2)
+N = 50
+X, y = make_blobs(n_samples=N, centers=2, n_features=2, random_state=0)
 y[y==0] = -1
 
 # plot examples
 y_plot = y.reshape(-1, 1)
 dataframe = pd.DataFrame(data=np.hstack([X,y_plot]), columns=("x", "y", "label"))
 sn.FacetGrid(dataframe, hue="label", size=6).map(plt.scatter, 'x', 'y').add_legend()
-plt.scatter(X[2][0], X[2][1], color='r')
-plt.scatter(X[44][0], X[44][1], color='r')
 plt.show()
 
 ## optimization
@@ -46,12 +44,10 @@ cons = {'type':'eq', 'fun': con}
 
 options = {'maxiter': 10}
 
-# res = minimize(func, alphs0, (X, y), tol=1e-2, bounds=bounds)
-# res = minimize(func, alphs0, (X, y), method='SLSQP', tol=1e-5, bounds=bounds, constraints=cons,
-#                options=options)
+
 res = minimize(func, alphs0, (X, y), tol=1e-5, bounds=bounds, constraints=cons,
                options=options)
-# res = minimize(func, alphs0, (X, y), tol=1e-2)
+
 
 ## draw decision boundary
 
@@ -68,8 +64,15 @@ alphs_opt = np.round(res.x.reshape((-1,1)), 2)
 w = np.zeros((2,1))
 for i in range(len(y)):
     w += (alphs_opt[i]*y[i]*X[i]).reshape(2,1)
+
+sup_vects = []  
+for i in np.nonzero(alphs_opt)[0]:
+    sup_vects.append((X[i], y[i]))
     
-b = -(w.T@X[2] + w.T@X[44])[0]/2
+if sup_vects[0][1] != sup_vects[1][1]:
+    b = -(w.T@sup_vects[0][0] + w.T@sup_vects[1][0])[0]/2
+else:
+    b = -(w.T@sup_vects[0][0]  + w.T@sup_vects[2][0])[0]/2
 
 for i in range(len(xx)):
     for j in range(len(yy)):
@@ -81,10 +84,14 @@ dataframe = pd.DataFrame(data=np.hstack([np.vstack((xxx.ravel(), yyy.ravel())).T
 sn.FacetGrid(dataframe, hue="label", palette='pastel', size=6).map(plt.scatter, 'x', 'y').add_legend()
 dataframe = pd.DataFrame(data=np.hstack([X,y_plot]), columns=("x", "y", "label"))
 sn.FacetGrid(dataframe, hue="label", size=6).map(plt.scatter, 'x', 'y').add_legend()
-plt.scatter(X[2][0], X[2][1], color='r')
-plt.scatter(X[44][0], X[44][1], color='r')
-plt.scatter(w[0], w[1], color='green', s=100)
+
+for sp in sup_vects:
+    plt.scatter(sp[0][0], sp[0][1], color='r')
+
+# plt.scatter(w[0], w[1], color='green', s=100)
 x_min, x_max = plt.xlim()
+y_min, y_max = plt.ylim()
 plt.plot((x_min, x_max), ((-b-w[0]*x_min)/w[1], (-b-w[0]*x_max)/w[1]), color='red')
+plt.ylim(y_min, y_max)
 plt.show()
 
