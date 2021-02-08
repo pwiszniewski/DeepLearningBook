ec78ee95b3f87ab9b08879203cabbe14706af2e3
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-02-08 20:30:25 +0100

    svm kernels

--- 5_machine_learning_basics/5_7_2_SVM_cvxopt_kernel.py
+++ 5_machine_learning_basics/5_7_2_SVM_cvxopt_kernel.py
@@ -2,16 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sn
 import pandas as pd
-import torch
 
 from sklearn.datasets import make_blobs
 from scipy.optimize import minimize
 
-from scipy.optimize import NonlinearConstraint
+from cvxopt import matrix, solvers
 
 ## make blobs
 N = 50
-X, y = make_blobs(n_samples=N, centers=2, n_features=2, random_state=0)
+X, y = make_blobs(n_samples=N, centers=2, n_features=2, random_state=1)
 y[y==0] = -1
 
 # plot examples
@@ -23,31 +22,34 @@ plt.show()
 ## optimization
 alphs0 = np.ones_like(y)
 
-def func(alphs, X, y):
-    sum1 = sum(alphs)
-    N = len(alphs)
-    sum2 = 0
-    for i in range(N):
-        for j in range(N):
-            sum2 += alphs[i]*alphs[j]*y[i]*y[j]*X[i]@X[j]
-    sum2 /= 2
-    ret = sum1 - sum2
-    # print(ret)
-    return - ret
+Q = np.zeros((N, N))
 
-bounds = [[0, np.inf] for _ in range(N)]
+def poly_kernel(x1, x2, d=2):
+    return (x1@x2 + 1)**d
 
-def con(alphs):
-    return alphs.T @ y
-# nlc = NonlinearConstraint(con, 0, 0)
-cons = {'type':'eq', 'fun': con}
-
-options = {'maxiter': 10}
+def poly_kernel_num(x1, x2, d=2):
+    return x1 * x2 + d
 
 
-res = minimize(func, alphs0, (X, y), tol=1e-5, bounds=bounds, constraints=cons,
-               options=options)
 
+# for i in range(N):
+#     for j in range(N):
+#         Q[i][j] = y[i]*y[j]*poly_kernel(X[i],X[j])
+        
+for i in range(N):
+    for j in range(N):
+        Q[i][j] = y[i]*y[j]*X[i]@X[j]
+
+
+Q = matrix(Q)
+# p = -matrix(np.ones((N,1))/N)
+p = -matrix(np.ones((N,1)))
+G = -matrix(np.eye(N))
+h = -matrix(np.zeros((N,1)))
+A = matrix(y.reshape(1,N).astype('float64') )
+b = matrix(0.0)
+sol = solvers.qp(Q, p, G, h, A, b)
+alphs_opt = np.array(sol['x'])
 
 ## draw decision boundary
 
@@ -59,25 +61,34 @@ xxx, yyy = np.meshgrid(xx, yy, sparse=False)
 
 arr = np.zeros((len(xx),len(yy)))
 
-alphs_opt = np.round(res.x.reshape((-1,1)), 2)
+# alphs_opt = np.round(res.x.reshape((-1,1)), 2)
 
 w = np.zeros((2,1))
 for i in range(len(y)):
     w += (alphs_opt[i]*y[i]*X[i]).reshape(2,1)
 
 sup_vects = []  
-for i in np.nonzero(alphs_opt)[0]:
+for i in np.nonzero(np.round(alphs_opt, 4))[0]:
     sup_vects.append((X[i], y[i]))
     
 if sup_vects[0][1] != sup_vects[1][1]:
     b = -(w.T@sup_vects[0][0] + w.T@sup_vects[1][0])[0]/2
 else:
     b = -(w.T@sup_vects[0][0]  + w.T@sup_vects[2][0])[0]/2
+    
+
 
 for i in range(len(xx)):
     for j in range(len(yy)):
-        arr[i][j] = (w.T@np.array([xxx[i][j], yyy[i][j]]) + b) > 0
-        # arr[i][j] = ((alphs_opt[i]*y[i]*X[i]@np.array([xxx[i][j], yyy[i][j]])) + b) > 0
+        for k in range(len(y)):
+            # arr[i][j] = (w.T@np.array([xxx[i][j], yyy[i][j]]) + b) > 0
+            arr[i][j] += alphs_opt[k]*y[k]*np.array([xxx[i][j], yyy[i][j]])@X[k]
+            # w += (alphs_opt[i]*y[i]*X[i]).reshape(2,1)
+        arr[i][j] = (arr[i][j] + b) > 0
+        # arr[i][j] = (w.T@np.array([xxx[i][j], yyy[i][j]]) + b) > 0
+            
+            
+        # arr[i][j] = (w.T@np.array(poly_kernel_num(xxx[i][j], yyy[i][j], 2)) + b) > 0
     
     
 dataframe = pd.DataFrame(data=np.hstack([np.vstack((xxx.ravel(), yyy.ravel())).T, arr.ravel().reshape(-1, 1)]), columns=("x", "y", "label"))
