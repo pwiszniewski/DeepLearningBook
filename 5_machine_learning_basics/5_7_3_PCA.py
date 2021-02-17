eff4fd5afbe08d6afccedb415e0c8b282e7adabb
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-02-17 20:30:18 +0100

    add PCA

---
+++ 5_machine_learning_basics/5_7_3_PCA.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_blobs
+
+n_samples = 500
+random_state = 170
+X, y = make_blobs(n_samples=n_samples, random_state=random_state, centers=1)
+
+# Anisotropicly distributed data
+transformation = [[0.60834549, -0.63667341], [-0.40887718, 1.85253229]]
+X_aniso = np.dot(X, transformation)
+#y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_aniso)
+
+plt.scatter(X_aniso[:, 0], X_aniso[:, 1])
+plt.title("Anisotropicly Distributed Blobs")
+
