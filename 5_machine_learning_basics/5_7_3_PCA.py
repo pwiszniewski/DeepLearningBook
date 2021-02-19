9d3d1c9ea61997c90b3f95d195b6d6a6945d0d7f
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-02-19 19:19:31 +0100

    eigen decomposition

--- 5_machine_learning_basics/5_7_3_PCA.py
+++ 5_machine_learning_basics/5_7_3_PCA.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
 n_samples = 500
-random_state = 170
+random_state = 127
 X, y = make_blobs(n_samples=n_samples, random_state=random_state, centers=1)
 
 # Anisotropicly distributed data
 transformation = [[0.60834549, -0.63667341], [-0.40887718, 1.85253229]]
 X_aniso = np.dot(X, transformation)
+X_aniso[:,1] = X_aniso[:,1] - np.mean(X_aniso[:,1])
 #y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_aniso)
 
 plt.scatter(X_aniso[:, 0], X_aniso[:, 1])
