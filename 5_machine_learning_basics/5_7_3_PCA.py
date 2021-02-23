84d75aedf1a334d63238e5f9c9e3033f8fdd2776
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-02-23 19:52:10 +0100

    try fix pca

--- 5_machine_learning_basics/5_7_3_PCA.py
+++ 5_machine_learning_basics/5_7_3_PCA.py
@@ -4,14 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
 n_samples = 500
-random_state = 127
-X, y = make_blobs(n_samples=n_samples, random_state=random_state, centers=1)
+random_state = 12
 
-# Anisotropicly distributed data
-transformation = [[0.60834549, -0.63667341], [-0.40887718, 1.85253229]]
-X = np.dot(X, transformation)
-X[:,1] = X[:,1] - np.mean(X[:,1])
-#y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_aniso)
+# X, y = make_blobs(n_samples=n_samples, random_state=random_state, centers=1)
+# transformation = [[0.60834549, -0.63667341], [-0.40887718, 1.85253229]]
+# X = np.dot(X, transformation)
+# X[:,1] = X[:,1] - np.mean(X[:,1])
+
+rng = np.random.RandomState(random_state)
+cov = [[3, 3],
+        [3, 4]]
+X = rng.multivariate_normal(mean=[0, 0], cov=cov, size=n_samples)
 
 plt.scatter(X[:, 0], X[:, 1])
 plt.title("Anisotropicly Distributed Blobs")
@@ -20,20 +23,25 @@ plt.show()
 eigval, eigvect = np.linalg.eig(X.T@X)
 
 D = eigvect
-X_pca = X @ D
-plt.scatter(X_pca[:, 0], X_pca[:, 1])
+print(D[0]@D[1])
+X_enc = X @ D
+X_dec = X_enc @ D.T
+plt.scatter(X_dec[:, 0], X_dec[:, 1])
 plt.title("Reconstructed - 2 eigenvencors")
 plt.show()
 
-D = eigvect[0]
-X_pca = X @ D
-plt.scatter(list(range(len(X_pca))), X_pca)
+D = eigvect[0].reshape(-1,1)
+X_enc = X @ D
+X_dec = X @ D @ D.T
+plt.scatter(X_dec[:, 0], X_dec[:, 1])
 plt.title(f"Reconstructed - 1 eigenvencors (eigenvalue: {eigval[0]:.1f})")
 plt.show()
 
-D = eigvect[1]
-X_pca = X @ D
-plt.scatter(list(range(len(X_pca))), X_pca)
+
+D = eigvect[1].reshape(-1,1)
+X_enc = X @ D
+X_dec = X @ D @ D.T
+plt.scatter(X_dec[:, 0], X_dec[:, 1])
 plt.title(f"Reconstructed - 1 eigenvencors (eigenvalue: {eigval[1]:.1f})")
 plt.show()
 
