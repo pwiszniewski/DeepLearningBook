3ecf126766c318f46bd5dfec64ba2936721364a4
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-03-15 16:24:46 +0100

    xor

--- 6_deep_feeedforward_networks/6_1_xor.py
+++ 6_deep_feeedforward_networks/6_1_xor.py
@@ -2,10 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-x = np.array([[0,0], [0,1], [1,0], [1,1]])
-w = np.random.rand(2, 1)
+X = np.array([[0,0], [0,1], [1,0], [1,1]])
+y = np.array([0, 1, 1, 0])
 
-b = np.random.randn(1)
-f = x @ w + b
+w = np.random.rand(3, 1) # [w0, w1, b]
+X = np.hstack((X, np.ones((X.shape[0], 1)))) # b constant
+
+f = X @ w
 
 J = np.mean(f)
+
+w_opt = np.linalg.inv(X.T @ X) @ (X.T @ y)
+
+# plot
+plt.scatter(X[:,0], X[:,1], c=y)
+xmin, xmax = plt.xlim()
+plt.plot((xmin, xmax), (.5, .5))
