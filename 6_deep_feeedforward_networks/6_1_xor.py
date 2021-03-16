11a15ff56616b088ca3035643afa2c6be4515394
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-03-16 17:41:10 +0100

    add mlp

--- 6_deep_feeedforward_networks/6_1_xor.py
+++ 6_deep_feeedforward_networks/6_1_xor.py
@@ -1,20 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 X = np.array([[0,0], [0,1], [1,0], [1,1]])
 y = np.array([0, 1, 1, 0])
 
-w = np.random.rand(3, 1) # [w0, w1, b]
-X = np.hstack((X, np.ones((X.shape[0], 1)))) # b constant
+''' Linear regression '''
 
-f = X @ w
+w = np.random.rand(3, 1) # [w0, w1, b]
+X_ext = np.hstack((X, np.ones((X.shape[0], 1)))) # b constant
+
+f = X_ext @ w
 
 J = np.mean(f)
 
-w_opt = np.linalg.inv(X.T @ X) @ (X.T @ y)
+w_opt = np.linalg.inv(X_ext.T @ X_ext) @ (X_ext.T @ y)
+y_hat_lr = X_ext @ w_opt
 
 # plot
-plt.scatter(X[:,0], X[:,1], c=y)
+plt.scatter(X_ext[:,0], X_ext[:,1], c=y)
 xmin, xmax = plt.xlim()
 plt.plot((xmin, xmax), (.5, .5))
+plt.show()
+
+''' MLP '''
+# function describing MKP is: f = w.T @ max{0, W.T@x + c} + b
+W = np.array([[1, 1], 
+              [1, 1]])
+c = np.array([0, -1])
+w = np.array([1, -2])
+b = 0
+
+h = X @ W + c
+
+# all points in hidden space lie on one line with slope 1
+# and cannot be separated linearly
+plt.scatter(h[:,0], h[:,1], c=y)
+xmin, xmax = plt.xlim()
+plt.show()
+
+# after ReLU the points are linearly separeble
+def ReLU(x):
+    return np.maximum(x, 0)
+h_rect = ReLU(h)
+plt.scatter(h_rect[:,0], h_rect[:,1], c=y)
+xmin, xmax = plt.xlim()
+plt.show()
+
+y_hat_lr = h_rect @ w
