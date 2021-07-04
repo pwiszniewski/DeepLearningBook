857ab1589f436e36a87395aca03c1dcac3aad6d1
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-07-04 10:44:57 +0200

    MLP

--- 6_deep_feeedforward_networks/6_1_xor.py
+++ 6_deep_feeedforward_networks/6_1_xor.py
@@ -20,9 +20,11 @@ y_hat_lr = X_ext @ w_opt
 plt.scatter(X_ext[:,0], X_ext[:,1], c=y)
 xmin, xmax = plt.xlim()
 plt.plot((xmin, xmax), (.5, .5))
+plt.title('Linear regression')
 plt.show()
 
 ''' MLP '''
+# simple MLP with 1 hidden layer consisting of 2 hidden units
 # function describing MKP is: f = w.T @ max{0, W.T@x + c} + b
 W = np.array([[1, 1], 
               [1, 1]])
@@ -36,6 +38,7 @@ h = X @ W + c
 # and cannot be separated linearly
 plt.scatter(h[:,0], h[:,1], c=y)
 xmin, xmax = plt.xlim()
+plt.title('MLP')
 plt.show()
 
 # after ReLU the points are linearly separeble
