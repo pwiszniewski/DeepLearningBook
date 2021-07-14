7fa16baa441867e4b29cd003bcea5075bb600c88
pwiszniewski <43343096+pwiszniewski@users.noreply.github.com>
2021-07-14 19:14:50 +0200

    add more features

--- 6_deep_feeedforward_networks/6_1_xor_1.py
+++ 6_deep_feeedforward_networks/6_1_xor_1.py
@@ -10,7 +10,9 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 a,b,c,d = 1,0,0,1   # coeffs of hidden layer matrix
 e,f = 0,0           # constants of hidden layer
 g, h = 0,0           # coeffs of final layer
-activation = 'ReLU'
+activation1 = 'ReLU'
+activation2 = 'None'
+out_type = 'binary'
 X = np.array([[0,0], [0,1], [1,0], [1,1]])
 y = np.array([0, 1, 1, 0])
 
@@ -23,6 +25,9 @@ y_grid = np.arange(-3.0,3.0,0.1)
 def ReLU(x):
     return np.maximum(x, 0)
 
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
+
 
 def draw_original(ax):
     global a, b, c, d
@@ -34,14 +39,17 @@ def draw_original(ax):
     X_grid,Y_grid = np.meshgrid(x_grid, y_grid) # grid of point    
     XX = np.concatenate([X_grid.reshape(1,-1), Y_grid.reshape(1,-1)], axis=0).T
     X_trans = XX @ W + C
-    if activation == 'ReLU':
+    if activation1 == 'ReLU':
         X_trans = ReLU(X_trans)
     w_out = np.array([g, h]).reshape(2, 1)
     out = X_trans @ w_out
-    ax.scatter(XX[:,0], XX[:,1], c=out > .5, cmap=plt.cm.Reds)
+    if activation2 == 'sigmoid':
+        out = sigmoid(out)
+    colors = out > .5 if out_type == 'binary' else out
+    ax.scatter(XX[:,0], XX[:,1], c=colors, cmap=plt.cm.Reds)
 
     X_trans = X @ W + C
-    if activation == 'ReLU':
+    if activation1 == 'ReLU':
         X_trans = ReLU(X_trans)
     ax.scatter(X[:,0], X[:,1], c=y)
 
@@ -56,14 +64,17 @@ def draw_transformed(ax):
     XX = np.concatenate([X_grid.reshape(1,-1), Y_grid.reshape(1,-1)], axis=0).T
     # print(np.concatenate([np.ravel(X_grid), np.ravel(Y_grid)]))
     X_trans = XX @ W + C
-    if activation == 'ReLU':
+    if activation1 == 'ReLU':
         X_trans = ReLU(X_trans)
     w_out = np.array([g, h]).reshape(2, 1)
     out = X_trans @ w_out
-    ax.scatter(X_trans[:,0], X_trans[:,1], c=out > .5, cmap=plt.cm.Reds)
+    if activation2 == 'sigmoid':
+        out = sigmoid(out)
+    colors = out > .5 if out_type == 'binary' else out
+    ax.scatter(X_trans[:,0], X_trans[:,1], c=colors, cmap=plt.cm.Reds)
 
     X_trans = X @ W + C
-    if activation == 'ReLU':
+    if activation1 == 'ReLU':
         X_trans = ReLU(X_trans)
     ax.scatter(X_trans[:,0], X_trans[:,1], c=y)
     #plt.contourf(x,y,z)
@@ -91,8 +102,12 @@ ax0.set_aspect(1)
 
 
 axcolor = 'lightgoldenrodyellow'
-rax = plt.axes([0.05, 0.7, 0.15, 0.15], facecolor=axcolor)
-radio = RadioButtons(rax, ('ReLU', 'None'))
+rax1 = plt.axes([0.05, 0.7, 0.15, 0.15], facecolor=axcolor)
+rax2 = plt.axes([0.05, 0.5, 0.15, 0.15], facecolor=axcolor)
+rax3 = plt.axes([0.05, 0.3, 0.15, 0.15], facecolor=axcolor)
+radio1 = RadioButtons(rax1, ('ReLU', 'None'))
+radio2 = RadioButtons(rax2, ('sigmoid', 'None'), active=1)
+radio3 = RadioButtons(rax3, ('binary', 'raw'))
 
 axa = plt.axes([0.25, 0.15, 0.3, 0.03], facecolor=axcolor)
 axb = plt.axes([0.25, 0.1, 0.3, 0.03], facecolor=axcolor)
@@ -217,9 +232,35 @@ def update_h(val):
     ax.set_ylim(-5, 5)
     fig.canvas.draw_idle()
 
-def switch_activation(label):
-    global activation
-    activation = label
+def switch_activation1(label):
+    global activation1
+    activation1 = label
+    ax0.clear()
+    ax.clear()    
+    draw_original(ax0)
+    draw_transformed(ax)
+    ax0.set_xlim(-5, 5)
+    ax0.set_ylim(-5, 5)
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+    fig.canvas.draw_idle()
+
+def switch_activation2(label):
+    global activation2
+    activation2 = label
+    ax0.clear()
+    ax.clear()    
+    draw_original(ax0)
+    draw_transformed(ax)
+    ax0.set_xlim(-5, 5)
+    ax0.set_ylim(-5, 5)
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+    fig.canvas.draw_idle()
+
+def switch_out_type(label):
+    global out_type
+    out_type = label
     ax0.clear()
     ax.clear()    
     draw_original(ax0)
@@ -239,7 +280,9 @@ se.on_changed(update_e)
 sf.on_changed(update_f)
 sg.on_changed(update_g)
 sh.on_changed(update_h)
-radio.on_clicked(switch_activation)
+radio1.on_clicked(switch_activation1)
+radio2.on_clicked(switch_activation2)
+radio3.on_clicked(switch_out_type)
 
 resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
